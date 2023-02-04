308edcfaca2d04cb1661779ffa33c520a2f6aff7
MichaelGreason <mgreason18@gmail.com>
2023-02-04 12:40:27 -0500

    made pictures circular and started grouping the address lines

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ function buildCustomerHtml(customer) {
 
     let img = document.createElement("img");
     img.src = (`${customer.picture.thumbnail}`);
+    img.classList.add("picture");
 
     let picture = document.createElement("h1");
 
@@ -39,27 +40,36 @@ function buildCustomerHtml(customer) {
     emailEl.appendChild(textEmail);
     customerDiv.appendChild(emailEl);
 
+    let addressBox = document.createElement("section")
     let addressEl = document.createElement("p");
-    addressEl.classList.add("addressElement");
+    addressBox.classList.add("addressElement");
 
     let textAddress = document.createTextNode(`${customer.location.street.number} ${customer.location.street.name}`);
     addressEl.appendChild(textAddress);
-    customerDiv.appendChild(addressEl);
+    addressBox.appendChild(addressEl)
+    customerDiv.appendChild(addressBox);
 
     let addressElTwo = document.createElement("p");
-    addressElTwo.classList.add("addressElement");
+    // addressElTwo.classList.add("addressElement");
 
     let textAddressTwo = document.createTextNode(`${customer.location.city}, ${customer.location.state} ${customer.location.postcode}`)
     addressElTwo.appendChild(textAddressTwo);
-    customerDiv.appendChild(addressElTwo);
+    addressBox.appendChild(addressElTwo)
+    // customerDiv.appendChild(addressElTwo);
 
-    // let dateOfBirthEl = document.createElement("p");
-    // dateOfBirthEl.classList.add("DOBelement");
+    let dateOfBirthEl = document.createElement("p");
+    dateOfBirthEl.classList.add("DOBelement");
 
-    // let textDob = document.createTextNode(`${moment(customer.dob.date).format("MMM Do YY")}`)
-    // dateOfBirthEl.appendChild(textDob);
-    // customerDiv.appendChild(dateOfBirthEl);
+    let textDob = document.createTextNode(`DOB: ${moment(customer.dob.date).format("MMM Do YYYY")}`)
+    dateOfBirthEl.appendChild(textDob);
+    customerDiv.appendChild(dateOfBirthEl);
 
+    let custSinceEl = document.createElement("p");
+    custSinceEl.classList.add("CustomerSince");
+
+    let textCustSince = document.createTextNode(`Customer since ${moment(customer.registered.date).format("MMM Do YYYY")}`);
+    custSinceEl.appendChild(textCustSince);
+    customerDiv.appendChild(custSinceEl);
 
 
 
